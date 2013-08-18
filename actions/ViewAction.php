4876ebf0431385d0e387a73574e236c272c30eb8
Jaap van der Kreeft <jaap@toxus.nl>
2013-08-18 16:17:26 +0200

    update actionView, toolbar

--- actions/ViewAction.php
+++ actions/ViewAction.php
@@ -8,6 +8,12 @@ class ViewAction extends CAction
 	public $form = null;		// the name of the form ex extension
 	public $defaultMode = 'view';
 	/**
+	 * extra parameters merged for the view
+	 * 
+	 * @var array
+	 */
+	public $params = array();
+	/**
 	 * 
 	 * @param type $id
 	 */
@@ -22,10 +28,14 @@ class ViewAction extends CAction
 		if (!empty($this->form)) {
 			$form = $this->controller->loadForm($this->form);
 		}
-		$this->controller->render($this->view, array(
-			'model' => $this->controller->model,
-			'form' => $form,	
-			'mode' => isset($_GET['mode']) ? $_GET['mode'] : $this->defaultMode,	
-		));
+		$params = array_merge(
+				array(
+					'model' => $this->controller->model,
+					'form' => $form,	
+					'mode' => isset($_GET['mode']) ? $_GET['mode'] : $this->defaultMode,					
+				),
+				$this->params
+		);				
+		$this->controller->render($this->view, $params);
 	}
 }
