7de410db3c9949dbfc2b378c88a3d67f9f72a831
Jaap van der Kreeft <jaap@toxus.nl>
2013-08-22 15:40:01 +0200

    added apache config

--- assetsBase/core/js/core.js
+++ assetsBase/core/js/core.js
@@ -34,3 +34,43 @@ $().ready(function() {
     $('#id-modal .modal_body').html('default_body');
 	});
 });	
+
+/*
+	 * Vibrating Alerts
+	 *
+	 * For more information, visit the project's homepage (http://prashant.es/labs/vibrating-notices/)
+	 *
+	 * Copyright (c) 2010 Prashant Sugand (prashant.es)
+	 * Licensed under the MIT license. http://www.opensource.org/licenses/mit-license.php
+	 */
+
+	jQuery.fn.vibrate = function(axis, distance, repetition, duration) {
+
+			var i = 0;
+			var o = distance / distance;
+
+			switch(axis) {
+				case 'x':
+					while(i < repetition) {
+						$(this).animate({marginLeft:'-'+distance+'px'}, duration);
+						$(this).animate({marginLeft:distance}, duration);
+						i++;
+						if(i == repetition) {
+							$(this).animate({marginLeft:o}, duration);
+						}
+					}
+					break;
+
+				case 'y':
+					while(i < repetition) {
+						$(this).animate({marginTop:'-'+distance+'px'}, duration);
+						$(this).animate({marginTop:distance}, duration);
+						i++;
+					}
+					break;
+			}
+	}		
+		
+	$().ready( function() {		
+		$('.shake').vibrate('x', 5, 3, 85);
+	});
