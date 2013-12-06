1bceab0b7ecf69e4adab1b37fd200f0ee9e0062a
Jaap van der Kreeft <jaap@toxus.nl>
2013-12-06 14:18:26 +0100

    fix: util.unique error

--- components/Util.php
+++ components/Util.php
@@ -14,7 +14,7 @@ class Util {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $string = '';    
     for ($p = 0; $p < $length; $p++) {
-        $string .= $characters[mt_rand(0, strlen($characters))];
+        $string .= $characters[mt_rand(0, strlen($characters)-1)];
     }
     return $string;
 	}
