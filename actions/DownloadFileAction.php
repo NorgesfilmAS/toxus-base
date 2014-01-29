a3d4d9adc470dd20ac724cbfc95b28c81b213588
Jaap van der Kreeft <jaap@toxus.nl>
2014-01-29 08:28:14 +0100

    add: DownloadAction Logging

--- actions/DownloadFileAction.php
+++ actions/DownloadFileAction.php
@@ -27,17 +27,22 @@ class DownloadFileAction extends BaseAction
 	 */
 	public function run($name='')
 	{
+		
 		$this->checkRights();
 		$filename = $this->path.$name;
+		Yii::log('Download '.$filename, CLogger::LEVEL_INFO, 'toxus.actions.DownloadFile');		
 		$ff = new FileInformation($filename);
 		if (!$ff->exists()) {
+			Yii::log('File does not exist '.$filename, CLogger::LEVEL_ERROR, 'toxus.actions.DownloadFile');		
 			throw new CHttpException(404, 'File not found');
 		}
 		if ($this->forceDownload) {
+			Yii::log('Force user to download.', CLogger::LEVEL_INFO, 'toxus.actions.DownloadFile');		
 			header('Content-disposition: attachment; filename='.$ff->filename);
 		}	
 		header('Content-type: '.$ff->contentType);
 		set_time_limit(0);
+		Yii::log('Sending file as '.$ff->contentType, CLogger::LEVEL_INFO, 'toxus.actions.DownloadFile');		
 		$file = @fopen($ff->path,"rb");
 		while(!feof($file))	{
 			@print(@fread($file, 1024*8));
@@ -45,6 +50,7 @@ class DownloadFileAction extends BaseAction
 			@flush();
 		}		
 		@fclose($file);		
+		Yii::log('File send', CLogger::LEVEL_INFO, 'toxus.actions.DownloadFile');		
 		Yii::app()->end(200);
 	}
 }
