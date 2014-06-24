55dad9ee583ceca91e4407595f9bdd562ce0004b
Jaap van der Kreeft <jaap@toxus.nl>
2014-06-24 12:40:02 +0200

    fix: site log

--- controllers/BaseSiteController.php
+++ controllers/BaseSiteController.php
@@ -86,6 +86,11 @@ class BaseSiteController extends Controller
 	 */
 	public function actionLog() 
 	{
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+		header('Access-Control-Max-Age: 1000');
+		header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+		
 	//	$path = YiiBase::getPathOfAlias('application.runtime').'/debug.'.time().'.log';
 		$fields = CJSON::decode(file_get_contents('php://input'));
 		$modelClass = $this->modelClass;
