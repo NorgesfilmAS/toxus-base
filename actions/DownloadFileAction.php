dbd2b9d532c5466f47b7fef81f8c645bd8affd1b
Jaap van der Kreeft <jaap@toxus.nl>
2014-07-29 15:34:19 +0200

    next

--- actions/DownloadFileAction.php
+++ actions/DownloadFileAction.php
@@ -69,13 +69,39 @@ class DownloadFileAction extends BaseAction
 		if ($this->userFilename === false) {
 			$this->userFilename = $ff->filename;
 		}
+		set_time_limit(0);
+		
+		$filesize = filesize($filename);
+
+		$offset = 0;
+		$length = $filesize;
+		if ( isset($_SERVER['HTTP_RANGE']) ) {
+			// if the HTTP_RANGE header is set we're dealing with partial content
+			$partialContent = true;
+			// find the requested range
+			// this might be too simplistic, apparently the client can request
+			// multiple ranges, which can become pretty complex, so ignore it for now
+			preg_match('/bytes=(\d+)-(\d+)?/', $_SERVER['HTTP_RANGE'], $matches);
+			$offset = intval($matches[1]);
+			$length = intval($matches[2]) - $offset;
+		} else {
+			$partialContent = false;
+		}		
+		
+		$file = @fopen($ff->path, "rb");
+		fseek($file, $offset);
+		if ( $partialContent ) {
+			header('HTTP/1.1 206 Partial Content');
+			header('Content-Range: bytes ' . $offset . '-' . ($offset + $length) . '/' . $filesize);
+		}
 		if ($this->forceDownload) {
 			header('Content-disposition: attachment; filename='.$this->userFilename);
 		}	
 		header('Content-type: '.$ff->contentType);
+		header('Content-Length: ' . $filesize);
+		header('Accept-Ranges: bytes');
 		header_remove("X-Powered-By");
-		set_time_limit(0);
-		$file = @fopen($ff->path, "rb");
+		
 		try {
 			while(!feof($file))	{
 				print(@fread($file, 1024*8));
